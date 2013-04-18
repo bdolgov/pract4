60aad82e915eb589cc8db8ee52b152e594c87cea
all3fox <all3fox@gmail.com>
2013-04-19 01:55:40 +0400

    fixed bug with row/column order

--- 04-1/main.cpp
+++ 04-1/main.cpp
@@ -1,46 +1,55 @@
-#include <iostream>
-#include <stdexcept>
-#include <memory>
 #include <vector>
+#include <memory>
+#include <utility>
+
+#include <iostream>
 
 template<typename T>
-class smart_pointer
+class smart_ptr final
 {
-    std::unique_ptr<T> pointer;
+    std::unique_ptr<T> p;
 public:
-    smart_pointer(T*);
-    smart_pointer& operator=(T*);
-    operator T*();
+    smart_ptr(T*);
+    
+    smart_ptr<T>& operator=(T*);
+
+    operator T* ();
+    operator T* () const;
 };
 
 template<typename T>
-smart_pointer<T>::smart_pointer(T* from)
-    : pointer(from)
-{}
+smart_ptr<T>::smart_ptr(T* t)
+    : p(t)
+{
+    std::cerr << "construted smart pointer" << std::endl;
+}
 
 template<typename T>
-smart_pointer<T>& smart_pointer<T>::operator=(T* target)
+smart_ptr<T>& smart_ptr<T>::operator=(T* q)
 {
-    pointer.reset(target);
+    p.reset(q);
     return *this;
 }
 
 template<typename T>
-smart_pointer<T>::operator T*()
+smart_ptr<T>::operator T* ()
 {
-    return pointer.get();
+    return p.get();
+}
+
+template<typename T>
+smart_ptr<T>::operator T* () const
+{
+    return p.get();
 }
 
 template<typename T>
 class PointerMatrix final
 {
-
-    int rows, cols;
-    std::vector<std::vector<smart_pointer<T>>> m;
+    int rows;
+    int cols;
+    std::vector<std::vector<smart_ptr<T>>> m;
 public:
-    static const int ROWS_MAX = 16384;
-    static const int COLS_MAX = 16384;
-
     PointerMatrix(int, int, T*);
 
     PointerMatrix(const PointerMatrix&) = delete;
@@ -50,62 +59,85 @@ public:
     int get_cols() const {return cols;}
 
     T* at(int, int) const;
-    smart_pointer<T>& at(int, int);
+    smart_ptr<T>& at(int, int);
+
+    static const int ROWS_MAX = 16384;
+    static const int COLS_MAX = 16384;
 };
 
 template<typename T>
-PointerMatrix<T>::PointerMatrix(int _rows, int _cols, T* t)
-    : rows(_rows), cols(_cols), m()
+PointerMatrix<T>::PointerMatrix(int inRows, int inCols, T* instance)
+    : rows(inRows), cols(inCols), m()
 {
-    if (_rows <= 0 or _cols <= 0 or _rows > ROWS_MAX
-        or _cols > COLS_MAX or t == nullptr) {
-        throw std::invalid_argument("Hello, sweetie!");
+    if (rows <= 0 or cols <= 0 or rows > ROWS_MAX or cols > COLS_MAX
+        or instance == nullptr) {
+            throw std::invalid_argument("Invalid argument");
     }
-    for (auto i = 0; i < _cols; ++i) {
-        m.emplace_back(std::vector<smart_pointer<T>>());
-        for (auto j = 0; j < _rows; ++j) {
-            m[i].emplace_back(smart_pointer<T>(t->clone() ));
+    for (auto i = 0; i < rows; ++i) {
+        m.emplace_back(std::vector<smart_ptr<T>>());
+        for (auto j = 0; j < cols; ++j) {
+            m[i].emplace_back(smart_ptr<T>(instance->clone()));
         }
     }
 }
 
 template<typename T>
-smart_pointer<T>& PointerMatrix<T>::at(int row, int col)
+T* PointerMatrix<T>::at(int row, int col) const
 {
-    if (row < 0 or col < 0 or row >= rows or col >= cols) {
-        throw std::range_error("Allons-y, Alonso!");
+    if (row < 0 or row >= rows or col < 0 or col >= cols) {
+        throw std::range_error("Out of range");
     }
     return m[row][col];
 }
 
 template<typename T>
-T* PointerMatrix<T>::at(int row, int col) const
+smart_ptr<T>& PointerMatrix<T>::at(int row, int col)
 {
-    if (row < 0 or col < 0 or row >= rows or col >= cols) {
-        throw std::range_error("Fantastic!");
+    if (row < 0 or row >= rows or col < 0 or col >= cols) {
+        throw std::range_error("Out of range");
     }
-    return m[row][col].get();
+    return m[row][col];
 }
 
-class Digit final
-{
-public:
-    int x;
-    Digit(int y) : x(y) {}
-    virtual ~Digit() {}
-    virtual Digit* clone() {
-        return new Digit(this->x);
-    }
-};
+// class Digit
+// {
+//     int x = 42;
+// public:
+//     Digit(int inX) : x(inX)
+//     {std::cerr << "creating another Digit" << std::endl;}
+//     ~Digit()
+//     {std::cerr << "another one bites the dust" << std::endl;}
 
-// #include <iostream>
+//     int getX() const {return x;}
+
+//     Digit* clone();
+// };
+
+// Digit* Digit::clone()
+// {
+//     std::cerr << "cloning" << std::endl;
+//     return new Digit(this->x);
+// }
 
 // int main()
 // {
-//     PointerMatrix<Digit> matrix(100, 100, &mydigit);
-//     matrix.at(0, 0) = &my_other_digit;
-//     // Digit* ppointer = matrix.at(0, 0);
-//     // std::cout << ppointer->x << std::endl;
-//     // matrix.at(0, 0) = &my_other_digit;
+//     Digit* digit = new Digit(43);
+//     Digit* funny = new Digit(1535);
+    
+//     try {
+//         PointerMatrix<Digit> matrix(0, 0, digit);
+//     } catch (std::invalid_argument e) {
+//         std::cerr << e.what() << std::endl;
+//     }
+
+//     PointerMatrix<Digit>* matrix = new PointerMatrix<Digit>(3, 3, digit);
+    
+//     Digit* test = matrix->at(0, 0);
+
+//     delete matrix;
+//     std::cerr << "test " << test->getX() << std::endl; 
+    
+//     delete funny;
+//     delete digit;
 //     return 0;
 // }
